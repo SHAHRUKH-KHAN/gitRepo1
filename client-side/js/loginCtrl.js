e1fa035e70e3dbb32708fe1cc404233486f32efe
SHAHRUKH-KHAN <shahrukh0603@gmail.com>
2015-04-25 15:01:05 +0530

    ctrl commit

--- client-side/js/loginCtrl.js
+++ client-side/js/loginCtrl.js
@@ -1,9 +1,18 @@
-'use strict'
 /*author Shahrukh */
-angular.module(app, []{
-
-
-})
-.controller {$scope, function(){
-
-}};
+angular.module("loginPage", []).controller("loginPageCtrl", function($scope) {
+    $scope.loginClickedOnce = false;
+    $scope.user = {};
+    var userDetails = [];
+    $scope.successfulLogin = false;
+    $scope.loginClicked = function() {
+        $scope.loginClickedOnce = true;
+    };
+    $scope.login = function() {
+        userDetails = $scope.user;
+        if (userDetails.password === "hello123") {
+            $scope.successfulLogin = true;
+        } else {
+            $scope.successfulLogin = false;
+        }
+    };
+});
